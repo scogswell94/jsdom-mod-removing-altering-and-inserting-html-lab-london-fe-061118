d99923cc07cd68a89aea706a285b73093835d155
sophiecogswell24@hotmail.com Cogswell <sophiecogswell24@hotmail.com>
2018-08-11 13:23:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-/* Enter the code to remove the main node element under this comment */
-
-/* Create your new element here and assign it to newHeader */
-const newHeader = null;
+document.queryselector('main').remove();
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+document.body.appendChild(newHeader);
+newHeader.innerHTML = 'Sophie';
